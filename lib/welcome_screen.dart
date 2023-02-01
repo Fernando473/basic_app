5391f1ff5e550a68d5484f39a3a6bb4e7fc1ae67
= <jhon.sangopanta@epn.edu.ec>
2023-02-01 00:13:04 -0500

    Main pages

---
+++ lib/welcome_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: Text('Welcome Screen'),
+        ),
+        backgroundColor: Colors.cyanAccent,
+      ),
+    );
+  }
+}
