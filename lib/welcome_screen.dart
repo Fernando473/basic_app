a4813863b1b2c9cf49115af63a82924d89343e28
= <jhon.sangopanta@epn.edu.ec>
2023-02-03 09:21:09 -0500

    Welcome Screen

--- lib/welcome_screen.dart
+++ lib/welcome_screen.dart
@@ -1,3 +1,5 @@
+import 'package:basic_app/login_screen.dart';
+import 'package:basic_app/sign_up_screen.dart';
 import 'package:flutter/material.dart';
 
 class WelcomeScreen extends StatelessWidget {
@@ -5,14 +7,103 @@ class WelcomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          child: Text('Welcome Screen'),
-        ),
-        backgroundColor: Colors.cyanAccent,
-      ),
-    );
+    return Material(
+        color: Colors.white,
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Image.asset('assets/welcome.png'),
+          const SizedBox(
+            height: 10,
+          ),
+          const Text(
+            "Welcome to mi first app",
+            style: TextStyle(
+              fontSize: 40,
+              fontWeight: FontWeight.bold,
+              color: Color(0xFF3F3D56),
+            ),
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          InkWell(
+            onTap: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const LoginScreen()));
+            },
+            child: Container(
+                width: 350,
+                padding: const EdgeInsets.symmetric(
+                  vertical: 15,
+                ),
+                decoration: BoxDecoration(
+                    color: const Color(0xFFFF9F00),
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Colors.black12,
+                        blurRadius: 4,
+                        spreadRadius: 2,
+                      ),
+                    ]),
+                child: const Center(
+                  child: Text(
+                    "Log in",
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                )),
+          ),
+          const Padding(
+            padding: EdgeInsets.symmetric(vertical: 10),
+            child: Text(
+              "OR",
+              style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w500,
+                  color: Color(0xFF3F3D56),
+                  fontStyle: FontStyle.italic),
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          InkWell(
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const SignUpScreen()));
+            },
+            child: Container(
+              width: 350,
+              padding: const EdgeInsets.symmetric(
+                vertical: 15,
+              ),
+              decoration: BoxDecoration(
+                  color: const Color(0xFF3F3D56),
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: const [
+                    BoxShadow(
+                      color: Colors.black12,
+                      blurRadius: 4,
+                      spreadRadius: 2,
+                    ),
+                  ]),
+              child: const Center(
+                child: Text(
+                  "Sing up",
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ]));
   }
 }
